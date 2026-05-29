083cac36205eb3cbbfe3971d2c58a03a7e5f2a78
Suryansh Shukla <suryanshshukla82@gmail.com>
2026-05-29 17:41:50 +0530

    fixed websocket url

--- chatting/settings.py
+++ chatting/settings.py
@@ -99,11 +99,10 @@ CHANNEL_LAYERS = {
     "default": {
         "BACKEND": "channels_redis.core.RedisChannelLayer",
         "CONFIG": {
-            "hosts": [("127.0.0.1", 6379)],
+            "hosts": [os.environ.get("REDIS_URL")],
         },
     },
 }
-
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
 
